1924646605ddcda47f189d7fe8bfdb1d586d23fb
Saad Essam <77013858+SaadEssam@users.noreply.github.com>
2021-07-09 22:57:39 +0200

    Add basic Models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, verbose_name=_("user"), on_delete=models.CASCADE)
+    name = models.CharField(_("الإسم :"), max_length=50)
+    who_i = models.TextField(_("نبذه عني :"), max_length=250)
+    price = models.IntegerField(_("سعر الكشف :"))
+
+    class Meta:
+        verbose_name = _("Profile")
+        verbose_name_plural = _("Profiles")
+
+
+    def __str__(self):
+        return self.name
+
