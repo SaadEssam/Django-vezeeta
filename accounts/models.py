e568fa501103d757a881b24a9d4d96cf174fd405
Saad Essam <77013858+SaadEssam@users.noreply.github.com>
2021-07-12 21:28:07 +0200

    Add Signals

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -8,7 +9,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, verbose_name=_("user"), on_delete=models.CASCADE)
     name = models.CharField(_("الإسم :"), max_length=50)
     who_i = models.TextField(_("نبذه عني :"), max_length=250)
-    price = models.IntegerField(_("سعر الكشف :"))
+    price = models.IntegerField(_("سعر الكشف :"), blank=True, null=True)
     image = models.ImageField(_("الصورة الشخصية"), upload_to='profile')
 
     class Meta:
@@ -17,5 +18,12 @@ class Profile(models.Model):
 
 
     def __str__(self):
-        return self.name
+        return '%s' %(self.user.username)
 
+
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        Profile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
